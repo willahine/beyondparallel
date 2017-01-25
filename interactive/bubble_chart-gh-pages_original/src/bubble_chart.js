091710640576c2b40afc3d6fa6d566ebccdf5646
willahine <whine@Willas-Mac-Pro.local>
2017-01-25 13:58:56 -0500

    18 range

--- interactive/bubble_chart-gh-pages_original/src/bubble_chart.js
+++ interactive/bubble_chart-gh-pages_original/src/bubble_chart.js
@@ -75,7 +75,7 @@ function bubbleChart() {
   // Sizes bubbles based on their area instead of raw radius
   var radiusScale = d3.scale.pow()
     .exponent(0.3)
-    .range([1, 15]);
+    .range([1, 18]);
 
   /*
    * This data manipulation function takes the raw data from
