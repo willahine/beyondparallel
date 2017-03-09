19e9e3f6ea1eecb9e1aa69edfa805fbd3dbde661
willahine <willa.hine@gmail.com>
2017-03-09 16:49:02 -0500

    Update bubble_chart.js

--- interactive/bubble_chart-gh-pages_original/src/bubble_chart.js
+++ interactive/bubble_chart-gh-pages_original/src/bubble_chart.js
@@ -21,7 +21,7 @@ function bubbleChart() {
   var center = { x: width / 2, y: height / 2 };
 
   var yearCenters = {
-    "zedong": {x: 2 * width / 6 + 50, y: height / 2 },
+    "zedong": {x: 1 * width / 6 + 90, y: height / 2 },
     "deng": { x: 3 * width / 6 + 50, y: height / 2 },
     "zemin": { x: 4 * width / 6 + 50, y: height / 2 },
     "jintao": { x: 5 * width / 6 + 25, y: height / 2 },
