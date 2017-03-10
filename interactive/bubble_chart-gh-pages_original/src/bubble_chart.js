8cf2f851459fab536504f1de303d38f38998dfe6
willahine <whine@Willas-Mac-Pro.local>
2017-03-10 11:47:38 -0500

    bubble location

--- interactive/bubble_chart-gh-pages_original/src/bubble_chart.js
+++ interactive/bubble_chart-gh-pages_original/src/bubble_chart.js
@@ -10,7 +10,7 @@
  */
 function bubbleChart() {
   // Constants for sizing
-  var width = 1200;
+  var width = 900;
   var height = 600;
 
   // tooltip for mouseover functionality
@@ -18,7 +18,7 @@ function bubbleChart() {
 
   // Locations to move bubbles towards, depending
   // on which view mode is selected.
-  var center = { x: width / 2, y: height / 2 };
+  var center = { x: width / 3, y: height / 3 };
 
   var yearCenters = {
     "zedong": {x:width / 6 + 95, y: height / 2 },
